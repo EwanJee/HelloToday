e8c92851b4fe682e5ee53808eed02f31aefcbcfe
Ewan Jee <830jiforcareer@gmail.com>
2025-07-02 17:17:38 +0900

    init: AI 서비스 클래스만 추가

---
+++ src/main/kotlin/web/hellotoday/ai/SpringAiService.kt
@@ -0,0 +1,10 @@
+package web.hellotoday.ai
+
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.stereotype.Service
+
+@Service
+class SpringAiService (
+    private val chatClient: ChatClient
+) {
+}
