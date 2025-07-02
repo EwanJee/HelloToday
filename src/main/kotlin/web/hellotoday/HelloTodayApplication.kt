1983e2b787033a1dd34e4e57a1165f046fcb9b43
Ewan Jee <830jiforcareer@gmail.com>
2025-07-02 17:16:39 +0900

    refactor: scheduling 활성화

--- src/main/kotlin/web/hellotoday/HelloTodayApplication.kt
+++ src/main/kotlin/web/hellotoday/HelloTodayApplication.kt
@@ -2,8 +2,10 @@ package web.hellotoday
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class HelloTodayApplication
 
 fun main(args: Array<String>) {
